f588f4de387c3c5e51dd4bd3bb82a4d4c83a17ca
agomb <angecque.go12@gmail.com>
2020-09-07 09:03:24 +0200

    Color change

--- obj.fs
+++ obj.fs
@@ -2,11 +2,12 @@
 precision mediump float;
 
 varying vec4 pos3D;
-varying vec3 N;
+varying vec3 N;	
+uniform vec3 Kd;
 
 void main(void)
 {
-	vec3 col = vec3(0.8,0.4,0.4) * dot(N,normalize(vec3(-pos3D))); // Lambert rendering, eye light source
+	vec3 col = Kd * dot(N,normalize(vec3(-pos3D))); // Lambert rendering, eye light source
 	gl_FragColor = vec4(col,1.0);
 }
 
